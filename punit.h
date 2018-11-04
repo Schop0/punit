4a0b58d8638f753b303c7376fb217e89d906b069
Peter Buijs <peter.buijs4045@gmail.com>
2018-11-04 22:24:02 +0100

    v0.1

---
+++ punit.h
@@ -0,0 +1,32 @@
+// Peter's unit testing framework for C
+
+#ifndef PUNIT
+#define PUNIT
+
+#include <stdio.h>
+
+extern int tests_run;
+extern int tests_failed;
+extern int asserts_failed;	// Gets reset by TEST()
+
+#define ASSERT(msg,condition)	\
+do{ \
+	if (!(condition)) { \
+		printf("\t%s\n", msg); \
+		asserts_failed++; \
+	}; \
+} while(0)
+
+#define TEST(test)	\
+do{ \
+	asserts_failed = 0; \
+	test(); \
+	tests_run++; \
+	if (asserts_failed) { \
+		tests_failed++; \
+	} else { \
+		printf("\tOK\n"); \
+	}; \
+} while(0)
+
+#endif
